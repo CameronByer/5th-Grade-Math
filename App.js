6092485f46964f18fe3c4d4119644619ea7bb709
CameronByer <chunecake@gmail.com>
2022-07-14 01:01:12 -0400

    Initial Commit
    Basic addition problems of increasing difficulty in relation to current streak

--- App.js
+++ App.js
@@ -1,11 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import {
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  TextInput,
+  View,
+} from 'react-native'
+
+const Question = () => {
+  const [answer, changeAnswer] = React.useState('');
+
+  const randomInt = (x, y) => {
+    return x + Math.floor((y-x+1)*Math.random());
+  }
+
+  const [correct, setcorrect] = React.useState(0);
+  const [a, seta] = React.useState(randomInt(10, 50));
+  const [b, setb] = React.useState(randomInt(10, 50));
+
+  const submitAnswer = () => {
+    let localCorrect = 0;
+    if ((a+b).toString() == answer) {
+      localCorrect = correct+1;
+      setcorrect(correct+1);
+    } else {
+      setcorrect(0);
+    }
+    seta(randomInt(10, 50*(localCorrect+1)));
+    setb(randomInt(10, 50*(localCorrect+1)));
+    changeAnswer('');
+  }
+
+
+  return (
+    <View>
+      <Text style={styles.bigCenter}>
+        {a} + {b}
+      </Text>
+      <TextInput
+        style={styles.bigCenter}
+        onChangeText={changeAnswer}
+        value={answer}
+        placeholder="Answer"
+        keyboardType="numeric"
+      />
+      <Button
+        onPress={submitAnswer}
+        title="Submit"
+        color="#841584"
+      />
+      <Text style={styles.bigCenter}>
+        Streak: {correct}
+      </Text>
+    </View>
+  );
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Question/>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +74,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  bigCenter: {
+    fontSize: 48,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  }
 });
