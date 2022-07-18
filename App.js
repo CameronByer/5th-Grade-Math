b984c518e6bc4db45a914d4c1275817048b6f596
CameronByer <chunecake@gmail.com>
2022-07-17 22:22:21 -0400

    Add rounding problems

--- App.js
+++ App.js
@@ -44,7 +44,7 @@ class Question extends Component {
     }
   }
 
-  generate(level=1, options=['addition', 'subtraction', 'multiplication', 'division']) {
+  generate(level=1, options=['addition', 'subtraction', 'multiplication', 'division', 'rounding']) {
     const type = choice(options);
     let question = '0+0';
     let answer = 0;
@@ -59,14 +59,39 @@ class Question extends Component {
       question = Math.max(x, y).toString() + '-' + Math.min(x, y).toString();
       answer = Math.abs(x - y);
     } else if (type == 'multiplication') {
-      const x = randomInt(2, 5+3*level);
-      const y = randomInt(2, 5+3*level);
+      const x = randomInt(2, 2+2*level);
+      const y = randomInt(2, 2+2*level);
       question = x.toString() + '*' + y.toString();
       answer = x * y;
     } else if (type == 'division') {
-      const x = randomInt(2, 5+3*level);
-      answer = randomInt(2, 5+3*level);
+      const x = randomInt(2, 2+2*level);
+      answer = randomInt(2, 2+2*level);
       question = (x*answer).toString() + '/' + x.toString();
+    } else if (type == 'rounding') {
+      const place = randomInt(-3, 3);
+      const negPlaceNames = ["ones", "tenths", "hundredths", "thousandths"];
+      const posPlaceNames = ["ones", "tens", "hundreds", "thousands"];
+      const placeName = place > 0 ? posPlaceNames[place] : negPlaceNames[-place];
+      const digits = (place > 0 ? randomInt(place+1, place+Math.floor(level/10)) : randomInt(2-place, 2-place + Math.floor(level/10)));
+      const decimalIndex = place > 0 ? 0 : randomInt(1, digits+place-1);
+      let decimalString = randomInt(1,9).toString();
+      for (let digit = 1; digit < digits; digit++) {
+        if (digit == decimalIndex) {
+          decimalString += '.';
+        }
+        decimalString += randomInt(digit != digits-1 ? 0 : 1, 9).toString();
+      }
+      if (decimalString.slice(-1) == "5") {
+        if (place > 0) {
+          decimalString += ".";
+        }
+        decimalString += randomInt(1,9).toString();
+      }
+      question = "Round " + decimalString + " to the " + placeName + " place";
+      answer = Math.round(parseFloat(decimalString)/10**place)/10**(-place);
+      if (place >= 0) {
+        answer = Math.round(answer)
+      }
     }
     return {'question': question, 'answer': answer}
   }
